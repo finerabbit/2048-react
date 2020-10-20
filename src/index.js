4eca0dc6bda479c07e6b86dcaff2f6dfc9289060
luckyrabbit870930 <luckyrabbit870930@gmail.com>
2020-10-21 05:06:30 +0900

    rendering tile.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Grid extends React.Component {
+class GridContainer extends React.Component {
 	render() {
 		return (
 			<div className="grid-container">
@@ -36,57 +36,55 @@ class Grid extends React.Component {
 }
 
 class Tile extends React.Component {
-	render () {
+	render() {
+		return (
+			<div className={this.props.tileCSS}>
+				<div className={this.props.tileInner}>{this.props.value}</div>
+			</div>
+		);
+	}
+}
+
+class TileContainer extends React.Component {
+	renderTile(i,value,style) {		// style 0 : normal, style 1 : new tile, style 2 : merged tile
+		const x = parseInt(i/4) + 1;
+		const y = i%4 + 1;
+		let tileCSS = "tile tile-" + value + " tile-position-" + x + "-" + y;
+
+		if (style === 1) {
+			tileCSS += " tile-new"
+		} else if (style === 2) {
+			tileCSS += " tile-merged"
+		}
+
+		return (
+			<Tile
+				tileCSS={tileCSS}
+				tileInner={"tile-inner"}
+				value={value}
+			/>
+		);
+	}
+
+	render() {
 		return (
 			<div className="tile-container">
-				<div className="tile tile-2 tile-position-1-1">
-					<div className="tile-inner">2</div>
-				</div>
-				<div className="tile tile-4 tile-position-1-2">
-					<div className="tile-inner">4</div>
-				</div>
-				<div className="tile tile-8 tile-position-1-3">
-					<div className="tile-inner">8</div>
-				</div>
-				<div className="tile tile-16 tile-position-1-4">
-					<div className="tile-inner">16</div>
-				</div>
-				<div className="tile tile-32 tile-position-2-1">
-					<div className="tile-inner">32</div>
-				</div>
-				<div className="tile tile-2048 tile-position-2-2">
-					<div className="tile-inner">64</div>
-				</div>
-				<div className="tile tile-1024 tile-position-2-3">
-					<div className="tile-inner">1024</div>
-				</div>
-				<div className="tile tile-256 tile-position-2-4">
-					<div className="tile-inner">256</div>
-				</div>
-				<div className="tile tile-2 tile-position-3-1">
-					<div className="tile-inner">2</div>
-				</div>
-				<div className="tile tile-4 tile-position-3-2">
-					<div className="tile-inner">4</div>
-				</div>
-				<div className="tile tile-8 tile-position-3-3">
-					<div className="tile-inner">8</div>
-				</div>
-				<div className="tile tile-16 tile-position-3-4">
-					<div className="tile-inner">16</div>
-				</div>
-				<div className="tile tile-32 tile-position-4-1">
-					<div className="tile-inner">32</div>
-				</div>
-				<div className="tile tile-64 tile-position-4-2">
-					<div className="tile-inner">64</div>
-				</div>
-				<div className="tile tile-128 tile-position-4-3">
-					<div className="tile-inner">128</div>
-				</div>
-				<div className="tile tile-512 tile-position-4-4">
-					<div className="tile-inner">512</div>
-				</div>
+				{this.renderTile(0,1024)}
+				{this.renderTile(1,2,1)}
+				{this.renderTile(2,4)}
+				{this.renderTile(3,16)}
+				{this.renderTile(4,32)}
+				{this.renderTile(5,64)}
+				{this.renderTile(6,1024)}
+				{this.renderTile(7,256)}
+				{this.renderTile(8,2)}
+				{this.renderTile(9,4)}
+				{this.renderTile(10,2048)}
+				{this.renderTile(11,8)}
+				{this.renderTile(12,128)}
+				{this.renderTile(13,512)}
+				{this.renderTile(14,4096,2)}
+				{this.renderTile(15,32)}
 			</div>
 		);
 	}
@@ -96,8 +94,8 @@ class Game extends React.Component {
 	render() {
 		return (
 			<div className="game-container">
-				<Grid />
-				<Tile />
+				<GridContainer />
+				<TileContainer />
 			</div>
 		);
 	}
